9c8275d1edf3cc51b37c34c667120fe95678f7bd
HH6683 <huanghepua@gmail.com>
2025-05-18 18:44:48 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,11 @@
 // pages/_app.js
 import Head from 'next/head';
 import { SessionProvider } from 'next-auth/react';
-import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* 引用 public/ 目录下的 favicon.ico */}
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <SessionProvider session={pageProps.session}>
